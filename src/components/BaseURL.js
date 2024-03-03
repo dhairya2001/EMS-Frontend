7d060fb7155f0b6d061b662db45bde01582e0ced
dhairya2001 <dhairyachhabra3072@gmail.com>
2024-03-03 18:11:38 +0530

    backend url

--- src/components/BaseURL.js
+++ src/components/BaseURL.js
@@ -1,1 +1,2 @@
-export const BaseURL="http://localhost:5000";
+export const BaseURL="https://ems-backend-f5l2tfvtn-dhairyas-projects-08fd99bd.vercel.app";
+// export const BaseURL="http://localhost:5000"
